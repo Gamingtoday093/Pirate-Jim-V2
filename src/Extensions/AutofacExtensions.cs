4e15176d6113aa8e402933afc8cf2fafaeb1b853
Gamingtoday093 <Gamingtoday093@gmail.com>
2026-06-11 19:48:05 +0200

    Refactored Services using Autofac

---
+++ src/Extensions/AutofacExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Autofac.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+
+namespace SomeCatIDK.PirateJim.Extensions;
+
+public static class AutofacExtensions
+{
+    public static IRegistrationBuilder<TLimit, TActivatorData, TStyle> InstancePerServiceLifetime<TLimit, TActivatorData, TStyle>
+        (this IRegistrationBuilder<TLimit, TActivatorData, TStyle> registration, ServiceLifetime serviceLifetime)
+    {
+        switch (serviceLifetime)
+        {
+            case ServiceLifetime.Singleton:
+                registration.SingleInstance();
+                break;
+            case ServiceLifetime.Scoped:
+                registration.InstancePerLifetimeScope();
+                break;
+            case ServiceLifetime.Transient:
+                registration.InstancePerDependency();
+                break;
+        }
+        return registration;
+    }
+}
